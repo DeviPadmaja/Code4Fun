3e78e21216c189472ca7033f3faf5f82ef62a7b8
karri <kvrkredd@ymail.com>
2014-09-29 20:27:20 +0530

    with exception

--- python/fabfile.py
+++ python/fabfile.py
@@ -3,5 +3,17 @@ from fabric.api import local
 def hello(name="world"):
     print "Hello %s" % (name)
 
+def startCry():
+    print "STARTING"
+
+def endCry():
+    print "WooHoo !! finished"
+
 def prepare():
     local("python echo.py")
+    raise Exception("yo")
+
+def do_task():
+    startCry()
+    prepare()
+    endCry()
