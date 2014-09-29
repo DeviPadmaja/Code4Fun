1cf89123ecde4359df0d8068f129d07d4d6156d5
karri <kvrkredd@ymail.com>
2014-09-29 20:22:32 +0530

    local tasks

---
+++ python/echo.py
@@ -0,0 +1,3 @@
+import time
+
+print "ECHO : time : %d" % (time.time())
--- python/fabfile.py
+++ python/fabfile.py
@@ -1,2 +1,7 @@
+from fabric.api import local
+
 def hello(name="world"):
     print "Hello %s" % (name)
+
+def prepare():
+    local("python echo.py")
